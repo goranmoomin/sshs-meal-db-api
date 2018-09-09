f52645ba91935c0b21a860c93567e44a4f2d02f0
Sungbin Jo <pcr910303@icloud.com>
2018-09-10 08:56:32 +0900

    Save to DB with sshs-meal-api info when DB is empty

--- app/route/today.js
+++ app/route/today.js
@@ -1,21 +1,30 @@
 const fsPromises = require("fs").promises;
 const assert = require("assert");
+const fetch = require("node-fetch");
 
 module.exports = async ctx => {
     const tmpdate = new Date();
     const date = new Date(tmpdate.getTime() + tmpdate.getTimezoneOffset() * 60000 + 9 * 3600000);
     const datestr = date.getFullYear().toString() + (date.getMonth() + 1).toString().padStart(2, "0");
+    const url = "mongodb://" + process.env.DB_USER + ":" + process.env.DB_PW + "@ds145562.mlab.com:45562/sshs-meal-api";
     let data;
     try {
         data = JSON.parse(await fsPromises.readFile("." + datestr, "utf-8"));
     } catch(e) {
         const MongoClient = ctx.MongoClient;
-        const client = await MongoClient.connect("mongodb://" + process.env.DB_USER + ":" + process.env.DB_PW + "@ds145562.mlab.com:45562/sshs-meal-api");
+        const client = await MongoClient.connect(url);
         const db = client.db("sshs-meal-api");
         const collection = db.collection(datestr);
 
         data = await collection.find().toArray();
-        fsPromises.writeFile("." + datestr, JSON.stringify(data));
+        if(data.length != 0) {
+            fsPromises.writeFile("." + datestr, JSON.stringify(data));
+        } else {
+            const res = await fetch("https://sshs-meal-api.now.sh/");
+            const monthlyData = await res.json();
+            collection.insertMany(data = monthlyData.message);
+        }
+        client.close();
     }
 
     ctx.body = {
