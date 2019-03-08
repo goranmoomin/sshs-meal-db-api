3ba03e371093bc437292c0b4d068942037dd7e0a
Sungbin Jo <pcr910303@icloud.com>
2019-03-08 14:26:33 +0900

    Change cache directory to `/tmp/sshs-meal`

--- app/route/today.js
+++ app/route/today.js
@@ -1,4 +1,5 @@
-const fsPromises = require("fs").promises;
+const { promises: fsPromises } = require("fs");
+const path = require("path");
 const assert = require("assert");
 const fetch = require("node-fetch");
 
@@ -8,20 +9,22 @@ module.exports = async ctx => {
     const datestr = date.getFullYear().toString() + (date.getMonth() + 1).toString().padStart(2, "0");
     let data;
     try {
-        data = JSON.parse(await fsPromises.readFile("." + datestr, "utf-8"));
+        data = JSON.parse(await fsPromises.readFile(path.join("/tmp", "sshs-meal", `.${datestr}`), "utf-8"));
     } catch(e) {
         const client = ctx.state.client;
         const db = client.db("sshs-meal-api");
         const collection = db.collection(datestr);
 
         data = await collection.find().toArray();
-        if(data.length != 0) {
-            fsPromises.writeFile("." + datestr, JSON.stringify(data));
-        } else {
+
+        if(data.length == 0) {
             const res = await fetch("https://sshs-meal-api.now.sh/");
             const monthlyData = await res.json();
             collection.insertMany(data = monthlyData.message);
         }
+
+        await fsPromises.mkdir(path.join("/tmp", "sshs-meal"));
+        await fsPromises.writeFile(path.join("/tmp", "sshs-meal", `.${datestr}`), JSON.stringify(data));
     }
 
     ctx.body = {
